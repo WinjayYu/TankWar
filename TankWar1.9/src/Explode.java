e3718339f047e73dd1813ab2cb7c5652cb4b8215
WinjayYu <winjayyu@gmail.com>
2015-09-09 14:58:17 +0800

    耽搁一些天后 继续更新

---
+++ TankWar1.9/src/Explode.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+
+public class Explode {
+	
+	int x, y;
+	private boolean live = true;
+
+	int i = 0;
+	
+	int [] diameter = {4, 6, 12, 20, 36, 50, 30, 10, 5};
+	
+	Tank t = null;
+	
+	public Explode(int x, int y, Tank t) {
+		this.x = x;
+		this.y = y;
+		this.t = t;
+	}
+	
+	public boolean isLive() {
+		return live;
+	}
+
+	public void setLive(boolean live) {
+		this.live = live;
+	}
+	
+	public void draw(Graphics g) {
+		
+		
+		if(!live) return;
+		if(i == diameter.length){
+			live = false;
+			i = 0;
+			return;
+		}
+		
+		Color c = g.getColor();
+		g.setColor(Color.ORANGE);
+		g.fillOval(x+CreateTank.WIDTH/2, y+CreateTank.HEIGHT/2, diameter[i], diameter[i]);
+		g.setColor(c);
+		
+		i ++;
+	}
+}
+
